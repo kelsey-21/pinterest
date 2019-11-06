413c3695efc41e0777aae2aa3a594d138ae4459b
Kelsey Creel <kelseycreel@gmail.com>
2019-11-05 21:26:22 -0600

    WIP

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -4,15 +4,21 @@ import $ from 'jquery';
 
 const authDiv = $('#auth');
 const logoutNavbar = $('#navbar-logout');
+const showBoards = $('#board-zone');
+const welcome = $('#welcome');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       logoutNavbar.removeClass('hide');
       authDiv.addClass('hide');
+      showBoards.classList.removeClass('hide');
+      welcome.classList.addClass('hide');
     } else {
       logoutNavbar.addClass('hide');
       authDiv.removeClass('hide');
+      showBoards.classList.addClass('hide');
+      welcome.classList.removeClass('hide');
     }
   });
 };
